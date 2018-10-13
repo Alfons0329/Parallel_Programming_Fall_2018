9f124e8fcea4ef521b91f652272fbdd0b65f1ce7
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-10-13 22:18:14 +0800

    Finished Parallel Programming HW1, ready for writing some noted on md

--- HW1/pi.cpp
+++ HW1/pi.cpp
@@ -12,7 +12,29 @@ long long int* each_in_circle;
 
 void* monte_carlo_runner(void* args)
 {
+    long cur_thread = (long) args;
+    //printf("cur thread %d \n", cur_thread);
+   
+    float x = 0.0f, y = 0.0f;
+    float pi_estimated = 0.0f;
+    long long int in_circle = 0;
+    unsigned int seed = time(NULL);
+    
+    for (register long long int i = 0; i < num_tosses / cpu_cores; i++)
+    {
+        // x = rand() / ((float)RAND_MAX);
+        // y = rand() / ((float)RAND_MAX);
+        x = rand_r(&seed) / ((float)RAND_MAX); //thread-safe random number generator
+        y = rand_r(&seed) / ((float)RAND_MAX); //thread-safe random number generator
+        //printf("x=[%f] y=[%f] \n", x, y);
+        if (x * x + y * y <= 1)
+        {
+            in_circle++;
+        }
+    }
 
+    each_in_circle[cur_thread] = in_circle;
+    pthread_exit(EXIT_SUCCESS);
 }
 
 inline void single_thread_monte_carlo()
@@ -58,14 +80,29 @@ int main(int argc, char *argv[])
     each_in_circle = new long long int[cpu_cores]; //stores the in_circle count of each thread
     pthread_t thread_id[cpu_cores];
 
-    for (int i = 0; i < cpu_cores; i++)
+    for (int i = 0; i < cpu_cores; i++) //i is the current thread
     {
         pthread_create(&thread_id[i], NULL, monte_carlo_runner, (void *) i);
     }
+
+    gettimeofday(&start, 0);
     for (int i = 0; i < cpu_cores; i++)
     {
         pthread_join(thread_id[i], NULL);
     }
+    //calculate pi from # of cpu_cores threads
+    long long int sum = 0;
+    for (int i = 0; i < cpu_cores; i++)
+    {
+        sum += each_in_circle[i];
+    }
+    gettimeofday(&end, 0);
+    float pi_estimated = 4 * sum / ((float)num_tosses);
+    
+    sec = abs(end.tv_sec - start.tv_sec);
+    usec = abs(end.tv_usec - start.tv_usec);
+    printf("Pi: %f\n", pi_estimated);
+    printf("multithread thread ([%d] threads) elapsed time %f ms \n",cpu_cores ,sec * 1000.0f + usec / 1000.0f);
 
     return 0;
 }
