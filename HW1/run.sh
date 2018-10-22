46326a13eaf75c7c0d1dc29d92bf520990f0c930
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-10-22 21:27:19 +0800

    Use shell script to determine the speed

--- HW1/run.sh
+++ HW1/run.sh
@@ -7,12 +7,23 @@ make
 echo "---------------c  -----------------------"
 for i in $(seq 1 $run_times);
 do
-    ./pi_out.o $1 $2
+    start=`date "+%s%N"`
+    ./pi_out_single.o $cpu_cnt $n_tosses
+    end=`date "+%s%N"`
+    t1=$(($end-$start))
+    echo "1 thread, elapsed time $t1 ns"
+
+    start=`date "+%s%N"`
+    ./pi_out.o $cpu_cnt $n_tosses
+    end=`date "+%s%N"`
+    t2=$(($end-$start))
+    t3=$((t1/t2))
+    echo "$cpu_cnt threads, elapsed time $t2 ns, accelerate rate = $t3"
 done
-echo "---------------cpp-----------------------"
-for i in $(seq 1 $run_times);
-do
-    ./pi_out_cpp.o $1 $2
-done
+# echo "---------------cpp-----------------------"
+# for i in $(seq 1 $run_times);
+# do
+#     ./pi_out_cpp.o $cpu_cnt $n_tosses
+# done
 echo "-----------run $3 times test end---------"
 
