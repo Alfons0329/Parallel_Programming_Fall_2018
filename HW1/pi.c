bf75d5b621bc60ae676cef2ed137b6e0a962eec1
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-10-22 17:21:52 +0800

    Remove unnecessary lines

--- HW1/pi.c
+++ HW1/pi.c
@@ -21,15 +21,9 @@ void* monte_carlo_runner(void* args)
     unsigned int seed = time(NULL);
 
     for (register long long int i = num_tosses / cpu_cores; i >= 0; i--)
-    // for (register long long int i = 0; i < num_tosses / cpu_cores; i++)
     {
         x = rand_r(&seed) / ((float)RAND_MAX); //thread-safe random number generator
         y = rand_r(&seed) / ((float)RAND_MAX); //thread-safe random number generator
-
-        // x = fast_rand2(-1, 1) / (float) 1.0;
-        // y = fast_rand2(-1, 1) / (float) 1.0;
-
-        // printf("x=[%f] y=[%f] \n", x, y);
         if (x * x + y * y <= 1.0f)
         {
             in_circle++;
@@ -42,66 +36,22 @@ void* monte_carlo_runner(void* args)
     pthread_exit(EXIT_SUCCESS);
 }
 
-void single_thread_monte_carlo()
-{
-    float x = 0.0f, y = 0.0f;
-    float pi_estimated = 0.0f;
-    long long int in_circle=0;
-
-    for(register long long int i = 0; i < num_tosses; i++)
-    {
-        x = rand() / ((float)RAND_MAX);
-        y = rand() / ((float)RAND_MAX);
-        //printf("x=[%f] y=[%f] \n", x, y);
-        if( x*x + y*y <=1 )
-        {
-            in_circle++;
-        }
-    }
-
-    pi_estimated = 4 * in_circle / ((float)num_tosses);
-    printf("Pi: %f\n", pi_estimated);
-}
-
-
 int main(int argc, char* argv[])
 {
     cpu_cores=atoi(argv[1]);
     num_tosses=atoll(argv[2]);
-    printf("cpu cores %d, num_tosses %lld \n", cpu_cores, num_tosses);
-    srand(time(NULL));
-
-    //--------------------------single thread monte carlo-----------------------//
-    struct timeval start, end;
-
-    gettimeofday(&start, 0);
-    single_thread_monte_carlo(); //measure the time of single thread process
-    gettimeofday(&end, 0);
-
-    int sec = abs(end.tv_sec - start.tv_sec);
-    int usec = abs(end.tv_usec - start.tv_usec);
-    printf("single thread elapsed time %f ms \n", sec * 1000.0f + usec / 1000.0f);
-    //--------------------------multi thread monte carlo-----------------------//
-    total_in_circle = 0;
     pthread_t thread_id[cpu_cores];
     for (long i = 0; i < cpu_cores; i++) //i is the current thread
     {
         pthread_create(&thread_id[i], NULL, monte_carlo_runner, (void *) i);
     }
 
-    gettimeofday(&start, 0);
     for (long i = 0; i < cpu_cores; i++)
     {
         pthread_join(thread_id[i], NULL);
     }
-    gettimeofday(&end, 0);
 
     float pi_estimated = 4 * total_in_circle / ((float)num_tosses);
-    
-    sec = abs(end.tv_sec - start.tv_sec);
-    usec = abs(end.tv_usec - start.tv_usec);
     printf("Pi: %f\n", pi_estimated);
-    printf("multithread thread ([%d] threads) elapsed time %f ms \n",cpu_cores ,sec * 1000.0f + usec / 1000.0f);
-
     return 0;
 }
