0c180f548e65d7e343abfc941b1a0f6bf8abf764
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-11-11 00:11:05 +0800

    Added HW3 prototypew

---
+++ HW3/prime.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <mip.h>
+
+int isprime(int n)
+{
+    int i, squareroot;
+    if (n > 10)
+    {
+        squareroot = (int)sqrt(n);
+        for (i = 3; i <= squareroot; i = i + 2)
+            if ((n % i) == 0)
+                return 0;
+        return 1;
+    }
+    else
+        return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int pc,       /* prime counter */
+        foundone; /* most recent prime found */
+    long long int n, limit;
+
+
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    sscanf(argv[1], "%llu", &limit);
+    printf("Starting. Numbers to be scanned= %lld\n", limit);
+
+    pc = 4; /* Assume (2,3,5,7) are counted here */
+
+    for (n = 11; n <= limit; n = n + 2)
+    {
+        if (isprime(n))
+        {
+            pc++;
+            foundone = n;
+        }
+    }
+    printf("Done. Largest prime is %d Total primes %d\n", foundone, pc);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 * Parallel Programming Lectured by Yi-Ping You @CS NCTU Taiwan
 * Course page: [Here](people.cs.nctu.edu.tw/~ypyou/courses/PP-f18/)
 # Homeworks
-* Parallelization computing of Pi estimation with Monte Carlo Method
-    * Mainly use the pthread library
+* Computing of Pi estimation with Monte Carlo Method [HW1 Repo](HW1/)
+    * Use the pthread library to accelerate the computation
+
+* Conjugate Gradient calculation [HW2 Repo](HW2/CG)
+    * Use the OpenMP library to accelerate the computation
