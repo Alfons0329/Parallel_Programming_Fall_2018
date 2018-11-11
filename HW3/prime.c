86015ab6e7d8f0e8bbdbd882ad12ed8ea42b545f
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-11-11 22:34:12 +0800

    Prime done, ready for verification and SSH keygen

--- HW3/prime.c
+++ HW3/prime.c
@@ -26,22 +26,24 @@ int main(int argc, char *argv[])
 {
     int 
         pc,       /* prime counter */
-        foundone; /* most recent prime found */
+        foundone, /* most recent prime found */
+        max_prime = 0,
+        total_prime = 0;
 
     long long int 
         n, /* number to start count on */ 
         limit /* upper size of count */
-        search_for_each_cpu; /* primes to search for eahc CPU */
 
 
-    int rank, /* CPU rank to identify which CPU is now used */ 
+    int 
+        rank, /* CPU rank to identify which CPU is now used */ 
         size; /* task size for each cpu */
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if(rank == 0)
+    if(rank == 0) /* master machine in the cluster */
     {
         sscanf(argv[1], "%llu", &limit);
         printf("Starting. Numbers to be scanned= %lld\n", limit);
@@ -50,6 +52,10 @@ int main(int argc, char *argv[])
 
         for (n = 11; n <= limit; n = n + 2)
         {
+            if(n & 1 == 0) /* even number will not be prime */
+            {
+                continue; 
+            }
             if (isprime(n))
             {
                 pc++;
@@ -57,13 +63,33 @@ int main(int argc, char *argv[])
             }
         }
     }
-    else
+    /* 
+     * slave machine example given for the MPI distributed computation
+     master search for 11 16
+     slave 1 search for 12 17
+     slave 2 search for 13 18
+     slave 3 search for 14 19
+     slave 4 search for 15 20
+     */
+    else if(rank > 0)
     {
-
+        for (n = 11 + rank; n <= limit; n+=rank)
+        {
+            if(n & 1 == 0) /* even number will not be prime */
+            {
+                continue; 
+            }
+            if (isprime(n))
+            {
+                pc++;
+                foundone = n;
+            }
+        }
     }
-
+    /* update the result */
+    MPI_Reduce(&pc, &total_prime, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&foundone, &total_prime, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
     printf("Done. Largest prime is %d Total primes %d\n", foundone, pc);
-
-    MPI_Finalize(); //terminate all distributive computation
+    MPI_Finalize(); /* terminate all distributive computation */
     return 0;
 }
