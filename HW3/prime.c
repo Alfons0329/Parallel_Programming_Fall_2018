fb4b9e3cac62812401e595002b931434e121a8d2
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-11-11 17:05:53 +0800

    Doing HW3 prime problem

--- HW3/prime.c
+++ HW3/prime.c
@@ -17,32 +17,53 @@ int isprime(int n)
     else
         return 0;
 }
-
+/* algorithm abstract
+ * distribute the total / N primes to count for each
+ * mod the number into range from 0 to N - 1
+ * the slave CPU #0 search the number K { K | K % N = 0 } to determine the prime, and the like
+ * */
 int main(int argc, char *argv[])
 {
-    int pc,       /* prime counter */
+    int 
+        pc,       /* prime counter */
         foundone; /* most recent prime found */
-    long long int n, limit;
+
+    long long int 
+        n, /* number to start count on */ 
+        limit /* upper size of count */
+        search_for_each_cpu; /* primes to search for eahc CPU */
 
 
-    int rank, size;
+    int rank, /* CPU rank to identify which CPU is now used */ 
+        size; /* task size for each cpu */
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    sscanf(argv[1], "%llu", &limit);
-    printf("Starting. Numbers to be scanned= %lld\n", limit);
 
-    pc = 4; /* Assume (2,3,5,7) are counted here */
-
-    for (n = 11; n <= limit; n = n + 2)
+    if(rank == 0)
     {
-        if (isprime(n))
+        sscanf(argv[1], "%llu", &limit);
+        printf("Starting. Numbers to be scanned= %lld\n", limit);
+
+        pc = 4; /* Assume (2,3,5,7) are counted <F4>here */
+
+        for (n = 11; n <= limit; n = n + 2)
         {
-            pc++;
-            foundone = n;
+            if (isprime(n))
+            {
+                pc++;
+                foundone = n;
+            }
         }
     }
+    else
+    {
+
+    }
+
     printf("Done. Largest prime is %d Total primes %d\n", foundone, pc);
 
+    MPI_Finalize(); //terminate all distributive computation
     return 0;
 }
