01b056bb0e41e462d62ea6ae698cbb95388a1380
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-11-12 14:17:26 +0800

    Debugging clustering computation error, problem 1 under working

--- HW3/run.sh
+++ HW3/run.sh
@@ -20,4 +20,5 @@ time ./prime_normal $nums
 #For the cluster system in the PP-f18 course
 echo "MPI version"
 read -p "Enter the numbers to be searched: " nums
-time /usr/lib64/openmpi/bin/mpiexec -n 3 -hostfile --map-by node ./prime
+time /usr/lib64/openmpi/bin/mpiexec -n 3 -hostfile hostfile --map-by node ./prime $nums
+#time /usr/lib64/openmpi/bin/mpiexec -n 3 -host pp1.cs.nctu.edu.tw, pp2.cs.nctu.edu.tw, pp3.cs.nctu.edu.tw ./prime $nums
