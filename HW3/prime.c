d480d6bb8811fea4248c03b4203b5448387c2748
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-11-14 15:07:20 +0800

    Check new architecture og prime

--- HW3/prime.c
+++ HW3/prime.c
@@ -41,8 +41,11 @@ int main(int argc, char *argv[])
     /*-------------------------------MPI data declaration starts--------------------------------*/
     int rank, /* CPU rank to identify which CPU is now used */ 
     size; /* task size for each cpu */
-    
-    struct prime_st myprime; /* prime data struct for MPI computing*/
+
+    struct prime_st sen, rcv; /* prime data struct for MPI computing, send and receive*/
+    sen.max_prime = 0;
+    sen.cnt = 4;
+
     MPI_Datatype datatype, oldtype[1]; /* only one datatype, so oldtype length be 1 */
     int blockcount[1];
     MPI_Aint offsets[1];
@@ -63,13 +66,46 @@ int main(int argc, char *argv[])
     MPI_Type_commit(&datatype);
     /*-------------------------------MPI init ends--------------------------------*/
 
-    if(rank == 0) /* master machine in the cluster */
+    if(rank == 0) /* master machine in the cluster send data to slave machine */
     {
         sscanf(argv[1], "%llu", &limit);
         printf("Starting. Numbers to be scanned= %lld\n", limit);
+
+        for (n = 11; n <= limit; n = n + 2)
+        {
+            if (isprime(n))
+            {
+                sen.max_prime = n;
+                sen.cnt++;
+            }
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            MPI_Send(&sen, 1, datatype, i, 1, MPI_COMM_WORLD);
+        }
+    }
+    
+    /* collect result from all the machines in the cluster and update */
+
+    for (int i = 0; i < size; i++)
+    {
+        MPI_Recv(&rcv, 1, datatype, i, 1, MPI_COMM_WORLD, &status);
+        sen.cnt += rcv.cnt;
+        
+        if(sen.max_prime < rcv.max_prime)
+        {
+            sen.max_prime = rcv.max_prime;
+        }
     }
 
+    /* terminate all distributive computation */
+    MPI_Type_free(&datatype);
+    MPI_Finalize(); 
 
-    MPI_Finalize(); /* terminate all distributive computation */
+    if (rank == 0)
+    {
+        printf("Done. Largest prime is %lld Total primes %lld\n", sen.max_prime, sen.cnt);
+    }
     return 0;
 }
