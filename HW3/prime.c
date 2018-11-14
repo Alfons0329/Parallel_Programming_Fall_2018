c3346ca37953d3fded96fabcf2f0f6efc28a9fb9
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-11-14 14:51:36 +0800

    Trying to improve algo

--- HW3/prime.c
+++ HW3/prime.c
@@ -18,76 +18,58 @@ int isprime(int n)
     {
         squareroot = (int)sqrt(n);
         for (i = 3; i <= squareroot; i = i + 2)
+        {
             if ((n % i) == 0)
+            {
                 return 0;
+            }
+        }
         return 1;
     }
     else
+    {
         return 0;
+    }
 }
-/* algorithm from ppt p48*/
+/* referenece from https://computing.llnl.gov/tutorials/mpi/ */
 int main(int argc, char *argv[])
 {
-    int 
-        pc,       /* prime counter */
-        foundone, /* most recent prime found */
-        total_prime = 0;
 
-    long long int 
-        n, /* number to start count on */ 
-        limit; /* upper size of count */
+    long long int n, /* number to start count on */ 
+    limit; /* upper size of count */
 
+    /*-------------------------------MPI data declaration starts--------------------------------*/
+    int rank, /* CPU rank to identify which CPU is now used */ 
+    size; /* task size for each cpu */
+    
+    struct prime_st myprime; /* prime data struct for MPI computing*/
+    MPI_Datatype datatype, oldtype[1]; /* only one datatype, so oldtype length be 1 */
+    int blockcount[1];
+    MPI_Aint offsets[1];
+    MPI_Status status;
+    /*-------------------------------MPI data declaration ends--------------------------------*/
 
-    int 
-        rank, /* CPU rank to identify which CPU is now used */ 
-        size; /* task size for each cpu */
-
+    /*-------------------------------MPI init starts--------------------------------*/
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    blockcount[0] = 2; /* long long gots two: max_prime and cnt */
+    offset[0] = 0; /* No offsets is needed */
+    oldtype[0] = MPI_LONG_LONG_INT;
+
+    /* define structured type and commit it */
+    MPI_Type_create_struct(1, blockcount, offsets, oldtype, &datatype);
+    MPI_Type_commit(&datatype);
+    /*-------------------------------MPI init ends--------------------------------*/
 
     if(rank == 0) /* master machine in the cluster */
     {
         sscanf(argv[1], "%llu", &limit);
         printf("Starting. Numbers to be scanned= %lld\n", limit);
-
-        pc = 4; /* Assume (2,3,5,7) are counted <F4>here */
-
-        for (n = 11; n <= limit; n = n + 2)
-        {
-            if(n & 1 == 0) /* even number will not be prime */
-            {
-                continue; 
-            }
-            if (isprime(n))
-            {
-                pc++;
-                foundone = n;
-            }
-        }
-        MPI_Reduce(&pc, &total_prime, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-        MPI_Reduce(&foundone, &total_prime, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
-        printf("Done. Largest prime is %d Total primes %d\n", foundone, pc);
-
     }
-    else if(rank > 0)
-    {
-        for (n = 11 + rank; n <= limit; n+=rank)
-        {
-            if(n & 1 == 0) /* even number will not be prime */
-            {
-                continue; 
-            }
-            if (isprime(n))
-            {
-                pc++;
-                foundone = n;
-            }
-        }
-        MPI_Reduce(&pc, &total_prime, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-        MPI_Reduce(&foundone, &total_prime, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
-    }
-    /* update the result */
+
+
     MPI_Finalize(); /* terminate all distributive computation */
     return 0;
 }
