25bb7cfc4e297c821aceb5d5d46ba3b56214a806
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-11-14 15:46:59 +0800

    Fixing clustering error

--- HW3/prime.c
+++ HW3/prime.c
@@ -36,11 +36,11 @@ int main(int argc, char *argv[])
 {
 
     long long int n, /* number to start count on */ 
-    limit; /* upper size of count */
+         limit; /* upper size of count */
 
     /*-------------------------------MPI data declaration starts--------------------------------*/
     int rank, /* CPU rank to identify which CPU is now used */ 
-    size; /* task size for each cpu */
+        size; /* task size for each cpu */
 
     struct prime_st sen, rcv; /* prime data struct for MPI computing, send and receive*/
     sen.max_prime = 0;
@@ -58,54 +58,50 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     blockcount[0] = 2; /* long long gots two: max_prime and cnt */
-    offset[0] = 0; /* No offsets is needed */
+    offsets[0] = 0; /* No offsets is needed */
     oldtype[0] = MPI_LONG_LONG_INT;
 
     /* define structured type and commit it */
     MPI_Type_create_struct(1, blockcount, offsets, oldtype, &datatype);
     MPI_Type_commit(&datatype);
     /*-------------------------------MPI init ends--------------------------------*/
+    printf("size = %d rank = %d \n", size, rank);
 
-    if(rank == 0) /* master machine in the cluster send data to slave machine */
+    for (n = 11 + size ; n <= limit ; n += 2 * size) 
     {
-        sscanf(argv[1], "%llu", &limit);
-        printf("Starting. Numbers to be scanned= %lld\n", limit);
-
-        for (n = 11; n <= limit; n = n + 2)
+        if (isprime(n)) 
         {
-            if (isprime(n))
+            sen.max_prime = n;
+            sen.cnt++;
+        }
+
+    }
+
+    if (rank != 0)
+    {
+        MPI_Send(&sen, 1, datatype, 0, 0, MPI_COMM_WORLD);
+    }
+    else 
+    {
+        for (int i = 1 ; i < size ; i++) 
+        {
+            MPI_Recv(&rcv, 1, datatype, i, 0, MPI_COMM_WORLD, &status);
+            sen.cnt += rcv.cnt;
+            if (sen.max_prime < rcv.max_prime)
             {
-                sen.max_prime = n;
-                sen.cnt++;
+                sen.max_prime = rcv.max_prime;
             }
         }
 
-        for (int i = 0; i < size; i++)
-        {
-            MPI_Send(&sen, 1, datatype, i, 1, MPI_COMM_WORLD);
-        }
-    }
-    
-    /* collect result from all the machines in the cluster and update */
-
-    for (int i = 0; i < size; i++)
-    {
-        MPI_Recv(&rcv, 1, datatype, i, 1, MPI_COMM_WORLD, &status);
-        sen.cnt += rcv.cnt;
-        
-        if(sen.max_prime < rcv.max_prime)
-        {
-            sen.max_prime = rcv.max_prime;
-        }
     }
 
-    /* terminate all distributive computation */
     MPI_Type_free(&datatype);
-    MPI_Finalize(); 
+    MPI_Finalize();
 
     if (rank == 0)
     {
-        printf("Done. Largest prime is %lld Total primes %lld\n", sen.max_prime, sen.cnt);
+        printf("Done. Largest prime is %lld Total primes %lld\n",sen.max_prime,sen.cnt);
     }
+
     return 0;
 }
