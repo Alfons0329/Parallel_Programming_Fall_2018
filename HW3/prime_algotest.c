09e9882228ab38324242235c11d2fdf94d02bda6
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-11-14 10:05:02 +0800

    Trying to improve algo

--- HW3/prime_algotest.c
+++ HW3/prime_algotest.c
@@ -1,8 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <stdbool.h>
 #define ll long long
+#define LLSPRP_MAX 7
+long long mod_mul(long long a, long long b, long long m)
+{
+    a %= m, b %= m;
+    long long y = (long long)((double)a * b / m + 0.5); /* fast for m < 2^58 */
+    long long r = (a * b - y * m) % m;
+    return r < 0 ? r + m : r;
+}
 
+long long powmod(long long a, long long b, long long mod)
+{ 
+    long long ans = 1;
+    for (; b; a = mod_mul(a, a, mod), b >>= 1)
+        if (b & 1)
+            ans = mod_mul(ans, a, mod);
+    return ans;
+}
+
+int sprp[3] = {2, 7, 61};                                           //int range
+int llsprp[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022}; //unsigned long long
+
+bool isprime(long long n, int *llsprp, int num)
+{
+    int t = 0;
+    long long u = n - 1;
+
+    for (; u % 2 == 0; ++t)
+    {
+        u >>= 1;
+    } 
+
+    for (int i = 0; i < num; ++i)
+    {
+        long long a = llsprp[i] % n;
+        if (a == 0 || a == 1 || a == n - 1)
+            continue;
+        long long x = powmod(a, u, n);
+        if (x == 1 || x == n - 1)
+            continue;
+        for (int j = 1; j < t; ++j)
+        {
+            x = mod_mul(x, x, n);
+            if (x == 1)
+                return 0;
+            if (x == n - 1)
+                break;
+        }
+        if (x == n - 1)
+            continue;
+        return 0;
+    }
+    return 1;
+}
 /*int prime[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
 ll mulmod(ll a, ll b, ll c)
@@ -77,12 +129,13 @@ int isprime(ll p, int iteration)
     }
     return 1;
 }*/
+/*
 int isprime(long long n)
 {
     long long sq = sqrt(n);
     if (n % 6 == 1 || n % 6 == 5)
     {
-        for (long long i = 5; i < sqrt(n); i = i + 2)
+        for (long long i = 3; i < sq; i = i + 2)
         {
             if (n % i == 0)
             {
@@ -92,7 +145,7 @@ int isprime(long long n)
         return 1;
     }
     return 0;
-}
+}*/
 int main(int argc, char *argv[])
 {
     int pc,       /* prime counter */
@@ -102,7 +155,7 @@ int main(int argc, char *argv[])
     sscanf(argv[1],"%llu",&limit);
     printf("Starting. Numbers to be scanned= %lld\n",limit);    
 
-    pc=4;     /* Assume (2,3,5,7) are counted here */
+    pc = 4;     /* Assume (2,3,5,7) are counted here */
 
     for (n = 11; n <= limit; n = n + 2) 
     {
@@ -110,7 +163,7 @@ int main(int argc, char *argv[])
         {
             continue;
         }
-        else if (isprime(n)) 
+        else if (isprime(n, llsprp, LLSPRP_MAX)) 
         {
             pc++;
             foundone = n;
