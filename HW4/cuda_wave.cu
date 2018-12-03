c438b1b44edabeee6a4dc8c12a5c34a196a9f678
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-12-03 18:04:03 +0800

    Doing cuda_wave

--- HW4/cuda_wave.cu
+++ HW4/cuda_wave.cu
@@ -31,27 +31,30 @@ float  values[MAXPOINTS+2], 	/* values at time t */
  *********************************************************************/
 void check_param(void)
 {
-   char tchar[20];
+    char tchar[20];
 
-   /* check number of points, number of iterations */
-   while ((tpoints < MINPOINTS) || (tpoints > MAXPOINTS)) {
-      printf("Enter number of points along vibrating string [%d-%d]: "
-           ,MINPOINTS, MAXPOINTS);
-      scanf("%s", tchar);
-      tpoints = atoi(tchar);
-      if ((tpoints < MINPOINTS) || (tpoints > MAXPOINTS))
-         printf("Invalid. Please enter value between %d and %d\n", 
-                 MINPOINTS, MAXPOINTS);
-   }
-   while ((nsteps < 1) || (nsteps > MAXSTEPS)) {
-      printf("Enter number of time steps [1-%d]: ", MAXSTEPS);
-      scanf("%s", tchar);
-      nsteps = atoi(tchar);
-      if ((nsteps < 1) || (nsteps > MAXSTEPS))
-         printf("Invalid. Please enter value between 1 and %d\n", MAXSTEPS);
-   }
+    /* check number of points, number of iterations */
+    while ((tpoints < MINPOINTS) || (tpoints > MAXPOINTS)) 
+    {
+        printf("Enter number of points along vibrating string [%d-%d]: "
+                ,MINPOINTS, MAXPOINTS);
+        scanf("%s", tchar);
+        tpoints = atoi(tchar);
+        if ((tpoints < MINPOINTS) || (tpoints > MAXPOINTS))
+            printf("Invalid. Please enter value between %d and %d\n", 
+                    MINPOINTS, MAXPOINTS);
+    }
 
-   printf("Using points = %d, steps = %d\n", tpoints, nsteps);
+    while ((nsteps < 1) || (nsteps > MAXSTEPS)) 
+    {
+        printf("Enter number of time steps [1-%d]: ", MAXSTEPS);
+        scanf("%s", tchar);
+        nsteps = atoi(tchar);
+        if ((nsteps < 1) || (nsteps > MAXSTEPS))
+            printf("Invalid. Please enter value between 1 and %d\n", MAXSTEPS);
+    }
+
+    printf("Using points = %d, steps = %d\n", tpoints, nsteps);
 
 }
 
@@ -60,22 +63,23 @@ void check_param(void)
  *********************************************************************/
 void init_line(void)
 {
-   int i, j;
-   float x, fac, k, tmp;
+    int i, j;
+    float x, fac, k, tmp;
 
-   /* Calculate initial values based on sine curve */
-   fac = 2.0 * PI;
-   k = 0.0; 
-   tmp = tpoints - 1;
-   for (j = 1; j <= tpoints; j++) {
-      x = k/tmp;
-      values[j] = sin (fac * x);
-      k = k + 1.0;
-   } 
+    /* Calculate initial values based on sine curve */
+    fac = 2.0 * PI;
+    k = 0.0; 
+    tmp = tpoints - 1;
+    for (j = 1; j <= tpoints; j++) 
+    {
+        x = k/tmp;
+        values[j] = sin (fac * x);
+        k = k + 1.0;
+    } 
 
-   /* Initialize old values array */
-   for (i = 1; i <= tpoints; i++) 
-      oldval[i] = values[i];
+    /* Initialize old values array */
+    for (i = 1; i <= tpoints; i++) 
+        oldval[i] = values[i];
 }
 
 /**********************************************************************
@@ -83,40 +87,49 @@ void init_line(void)
  *********************************************************************/
 void do_math(int i)
 {
-   float dtime, c, dx, tau, sqtau;
+    float dtime, c, dx, tau, sqtau;
 
-   dtime = 0.3;
-   c = 1.0;
-   dx = 1.0;
-   tau = (c * dtime / dx);
-   sqtau = tau * tau;
-   newval[i] = (2.0 * values[i]) - oldval[i] + (sqtau *  (-2.0)*values[i]);
+    dtime = 0.3;
+    c = 1.0;
+    dx = 1.0;
+    tau = (c * dtime / dx);
+    sqtau = tau * tau;
+    newval[i] = (2.0 * values[i]) - oldval[i] + (sqtau *  (-2.0)*values[i]);
 }
 
 /**********************************************************************
  *     Update all values along line a specified number of times
  *********************************************************************/
-void update()
+__global__ void update(float *values)
 {
-   int i, j;
+    int i, j;
 
-   /* Update values for each time step */
-   for (i = 1; i<= nsteps; i++) {
-      /* Update points along line for this time step */
-      for (j = 1; j <= tpoints; j++) {
-         /* global endpoints */
-         if ((j == 1) || (j  == tpoints))
-            newval[j] = 0.0;
-         else
-            do_math(j);
-      }
+    /*init line*/
 
-      /* Update old values with new values */
-      for (j = 1; j <= tpoints; j++) {
-         oldval[j] = values[j];
-         values[j] = newval[j];
-      }
-   }
+
+
+    int cuda_thread = threadIdx.x + 
+
+    /* Update values for each time step */
+    for (i = 1; i<= nsteps; i++) 
+    {
+        /* Update points along line for this time step */
+        for (j = 1; j <= tpoints; j++) 
+        {
+            /* global endpoints */
+            if ((j == 1) || (j  == tpoints))
+                newval[j] = 0.0;
+            else
+                do_math(j);
+        }
+
+        /* Update old values with new values */
+        for (j = 1; j <= tpoints; j++) 
+        {
+            oldval[j] = values[j];
+            values[j] = newval[j];
+        }
+    }
 }
 
 /**********************************************************************
@@ -124,13 +137,14 @@ void update()
  *********************************************************************/
 void printfinal()
 {
-   int i;
+    int i;
 
-   for (i = 1; i <= tpoints; i++) {
-      printf("%6.4f ", values[i]);
-      if (i%10 == 0)
-         printf("\n");
-   }
+    for (i = 1; i <= tpoints; i++) 
+    {
+        printf("%6.4f ", values[i]);
+        if (i%10 == 0)
+            printf("\n");
+    }
 }
 
 /**********************************************************************
@@ -138,16 +152,18 @@ void printfinal()
  *********************************************************************/
 int main(int argc, char *argv[])
 {
-	sscanf(argv[1],"%d",&tpoints);
-	sscanf(argv[2],"%d",&nsteps);
-	check_param();
-	printf("Initializing points on the line...\n");
-	init_line();
-	printf("Updating all points for all time steps...\n");
-	update();
-	printf("Printing final results...\n");
-	printfinal();
-	printf("\nDone.\n\n");
-	
-	return 0;
+    sscanf(argv[1],"%d",&tpoints);
+    sscanf(argv[2],"%d",&nsteps);
+    check_param();
+
+    printf("Initializing points on the line...\n");
+
+    init_line();
+    printf("Updating all points for all time steps...\n");
+    update();
+    printf("Printing final results...\n");
+    printfinal();
+    printf("\nDone.\n\n");
+
+    return 0;
 }
--- HW4/run.sh
+++ HW4/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+set -e
 
 make normal
-time ./serial_wave $1 $2
+time ./serial_wave $1 $2 > serial_result.txt
 
+make cuda
+time ./cuda_wave $1 $2 > cuda_result.txt
+
+cat serial_result.txt | grep real
+cat cuda_result.txt | grep real
