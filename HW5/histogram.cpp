54cc3ccc5c05b77e1e8f087daf080059d93e4cd9
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-12-12 15:08:54 +0800

    Kernel build OK, ready for the whole execution checking and validation

--- HW5/histogram.cpp
+++ HW5/histogram.cpp
@@ -8,59 +8,60 @@
  * Calculate the statistic data of the (R, G, B) of image, 
  * where each color can be describe as
  * 0xRRGGBB and the R_stat[RR]++; G_stat[GG]++; B_stat[BB]++;, after the iterations
- * the result can be shown as an image histogram
- * **********************************************************************************************************/
-
-/* OpenCL kernel function 
-    __kernel void histogram(__global unsigned char* image_data, __global unsigned int* result_data, unsigned int size)
-    {
-        int idx = get_global_id(0);
-        if(idx > size - 2) 
-        {
-            return;
-        }
-        result_data[idx] = image_data[idx];
-        result_data[idx + 1] = image_data[idx + 1];
-        result_data[idx + 2] = image_data[idx + 2];
-    }
+ * the result can be shown as an image histogram.
  *
- * */
-
-unsigned int * histogram(unsigned char *image_data, unsigned int _size) {
-
-    unsigned char *img = image_data;
-    unsigned int *ref_histogram_results;
-    unsigned int *ptr;
-
-    ref_histogram_results = (unsigned int *)malloc(256 * 3 * sizeof(unsigned int));
-    ptr = ref_histogram_results;
-    memset (ref_histogram_results, 0x0, 256 * 3 * sizeof(unsigned int));
-    // data encoded with R G B R G B R G B...
-    // histogram of R
-    for (unsigned int i = 0; i < _size; i += 3)
-    {
-        unsigned int index = img[i];
-        ptr[index]++;
-    }
-
-    // histogram of G
-    ptr += 256;
-    for (unsigned int i = 1; i < _size; i += 3)
-    {
-        unsigned int index = img[i];
-        ptr[index]++;
-    }
-
-    // histogram of B
-    ptr += 256;
-    for (unsigned int i = 2; i < _size; i += 3)
-    {
-        unsigned int index = img[i];
-        ptr[index]++;
-    }
-
-    return ref_histogram_results;
-}
+ * The data should ne parallelized corresponding to  [0, 255]for R [256, 512]for G [513, 767]for B
+ * **********************************************************************************************************/
+const char* histogram = "\ 
+__kernel void histogram(__global unsigned char* image_data, __global unsigned int* result_data, unsigned int size)\
+{\
+    int idx = get_global_id(0);\
+    int i;\
+    if(idx > size - 2) \
+    {\
+        return;\
+    }\
+    \
+    switch(idx)\
+    {\
+        case 0 ... 255:\
+            {\
+                for (i = 0; i < size; i+=3)\
+                {\
+                    if (idx == image_data[i])\
+                    {\
+                        result_data[idx]++;\
+                    }\
+                }\
+                break;\
+            }\
+        case 256 ... 511:\
+            {\
+                for (i = 1; i < size; i+=3)\
+                {\
+                    if (idx - 256 == image_data[i])\
+                    {\
+                        result_data[idx]++;\
+                    }\
+                }\
+                break;\
+            }\
+        case 512 ... 767:\
+            {\
+                for (i = 2; i < size; i+=3)\
+                {\
+                    if (idx - 512 == image_data[i])\ 
+                    {\
+                        result_data[idx]++;\
+                    }\
+                }\
+                break;\
+            }\
+        default:\
+            break;\
+    }\
+}\
+";
 
 int main(int argc, char const *argv[])
 {    
@@ -68,12 +69,12 @@ int main(int argc, char const *argv[])
     unsigned char* image; // For input image 
     unsigned int* histogram_results; // For output result
     unsigned int i = 0, a, input_size;
-    
+
     //--------------------file IO and allocate memory-----------//
     FILE* inFile = fopen("input", "r");
     FILE* outFile = fopen("0416324.out", "w");
     fscanf(inFile, "%u", &input_size);
-    
+
     image = (unsigned char* ) malloc (sizeof(unsigned char) * input_size);// R, G, B ranging from 0x00 to 0xFF
     histogram_results = (unsigned int* ) malloc (sizeof(unsigned int) * 256 * 3);// R 256, G 256, B 256, total 768 statistical data
     memset(histogram_results, 0, sizeof(sizeof(unsigned int) * 256 * 3));
@@ -132,17 +133,36 @@ int main(int argc, char const *argv[])
         printf("CL memory enqueueing failed\n");
         return 1;
     }
-    
+
     //-------------------kernel program execution-----------------//
-    histogram_results = histogram(image, input_size);
-    for(unsigned int i = 0; i < 256 * 3; ++i) 
+
+    size_t src_size = strlen(histogram);
+    cl_program kernel_prog = clCreateProgramWithSource(ctx, 1, (const char **) &histogram, &src_size, &cl_err); // create the kernel program for OpenCL architecture
+    cl_err2 = clBuildProgram(kernel_prog, 1, &device_id, NULL, NULL, NULL); // build the kernel program on device
+    cl_kernel kernel_core = clCreateKernel(kernel_prog, "histogram", &cl_err3); // create the kernel object
+    if (cl_err == CL_SUCCESS && cl_err2 == CL_SUCCESS && cl_err3 == CL_SUCCESS && cl_err4 == CL_SUCCESS)
+    {
+        printf("CL kernel build all OK \n");
+    }
+    else
+    {
+        printf("CL kernel build failed \n");
+        return 1;
+    }
+
+    //histogram_results = histogram(image, input_size);
+
+    /*for(unsigned int i = 0; i < 256 * 3; ++i) 
     {
         if (i % 256 == 0 && i != 0)
         {
             fprintf(outFile, "\n");
         }
         fprintf(outFile, "%u ", histogram_results[i]);
-    }
+    }*/
+
+    // clReleaseProgram();
+    // clReleaseKernel();
     fclose(inFile);
     fclose(outFile);
     return 0;
