75ebade960363b11532555a700b0e0860ebee7b9
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-12-13 15:37:26 +0800

    Update run shell and thoughts

--- Final Project/run.sh
+++ Final Project/run.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-make matrix
-make all
-./cm.o &&
-./gb_std.o
+set -e
 
+read -p "1: Standard, 2: pthread, 3: OpenMP, 4: CUDA, 5:Generate Gaussian Matrix 7: Clean output files " choose
+
+case $choose in
+    1)
+        make standard
+        ;;
+    2)
+        make pthread
+        ;;
+    3)
+        make omp
+        ;;
+    4)
+        make cuda
+        ;;
+    5)
+        make matrix
+        ./cm.o
+        ;;
+    7)
+        make clean
+        ;;
+esac
+python show.py $
 
