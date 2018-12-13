2b16520918cba28255446dec5afa534d1d1fdbf4
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-12-13 12:30:44 +0800

    Update profiler

--- HW5/profiler.sh
+++ HW5/profiler.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+function Test
+{
+ if [ $# -eq 0 ]
+ then
+     echo "At least 1 argument"
+ else
+     mkfifo input
+     cat $1 > input&
+     time ./histogram
+     diff $2.out "$1.out" > /dev/null
+     if [ $? -eq 0 ]
+     then
+         echo $1 is correct
+     fi
+     rm input $2.out
+ fi
+}
+
 if [ $# -eq 0 ]
 then
     echo "input Student ID"
@@ -6,7 +24,7 @@ else
     zip "HW5_$1.zip" histogram.cpp
     unzip -o "HW5_$1.zip"
     g++ -pg -o histogram histogram.cpp -lOpenCL
-    /home/net/test.sh $1
+    Test "/home/net/input-307M" $1
     gprof histogram gmon.out
 fi
 
