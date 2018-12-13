8a3df62d2a5b6f28c2c8d9dfd0aa064d8128fb01
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-12-13 21:05:05 +0800

    Doing CUDA Gaussian Blur

--- Final Project/gaussian_blur_cuda.cu
+++ Final Project/gaussian_blur_cuda.cu
@@ -55,7 +55,7 @@ __global__ void cuda_gaussian_filter(unsigned char* input_image, unsigned char* 
 			{
 				continue;
 			} 
-			tmp += filter_G[j * ws + i] * pic_in[3 * (b * img_width + a) + shift];
+			tmp += filter_G[j * ws + i] * input_image[3 * (b * img_width + a) + shift];
 		}
 	}
 
@@ -89,6 +89,8 @@ void write_and_show(BmpReader* bmpReader, string outputblur_name, int k)
 
 int main(int argc, char* argv[])
 {
+    TILE_WIDTH = 1024;
+
 	// read input filename
 	string inputfile_name;
     string outputblur_name;
@@ -120,30 +122,53 @@ int main(int argc, char* argv[])
 
     // main part of Gaussian blur
 	BmpReader* bmpReader = new BmpReader();
-	for (int k = 1; k < argc; k++)
+	
+    for (int k = 1; k < argc; k++)
 	{
-		// read input BMP file
-
+        
+        // read input BMP file
         inputfile_name = argv[k];
 		pic_in = bmpReader -> ReadBMP(inputfile_name.c_str(), &img_width, &img_height);
 	    printf("Filter scale = %f and image size W = %d, H = %d\n", FILTER_SCALE, img_width, img_height);
 
 		// allocate space for output image
-		pic_out = (unsigned char*)malloc(3 * img_width * img_height * sizeof(unsigned char));
+        int resolution = 3 * (img_width * img_height  + 1); //padding
+		pic_out = (unsigned char*)malloc(resolution * sizeof(unsigned char));
 
-		//apply the Gaussian filter to the image, RGB respectively
+		// apply the Gaussian filter to the image, RGB respectively
         string tmp(inputfile_name);
-        int segment_cnt = 1;
-		
+        
+        //---------------------CUDA main part-------------------------//
+        // allocate space
+        unsigned char* cuda_input_image;
+        unsigned char* cuda_output_image;
+
+        cudaMalloc((void**) &cuda_input_image, resolution * sizeof(unsigned char));
+        cudaMalloc((void**) &cuda_output_image, resolution * sizeof(unsigned char));
+
+        // copy memory from host to GPU
+        cudaMemcpy(cuda_input_image, pic_in, resolution * sizeof(unsigned char), cudaMemcpyHostToDevice);
+        //cudaMemcpy(cuda_output_image, pic_out, resolution * sizeof(unsigned char), cudaMemcpyHostToDevice);
+
+        for (int i = 0; i < 3; i++) //R G B channel respectively
+        {
+            cuda_gaussian_filter<<<(resolution + TILE_WIDTH) / TILE_WIDTH, TILE_WIDTH>>>(cuda_input_image, cuda_output_image, img_width, img_height, i);_
+        }
+
+        // copy memory from GPU to host
+        cudaMemcpy(pic_out, cuda_output_image, resolution * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+        //---------------------CUDA main part-------------------------//
         
         // write output BMP file
         outputblur_name = "input" + to_string(k) + "_blur.bmp";
 		bmpReader->WriteBMP(outputblur_name.c_str(), img_width, img_height, pic_out);
+        //write_and_show(bmpReader, inputfile_name, k);
 
-        write_and_show(bmpReader, inputfile_name, k);
 		// free memory space
 		free(pic_in);
 		free(pic_out);
+        cudaFree(cuda_input_image);
+        cudaFree(cuda_output_image);
 	}
 
 	return 0;
