f9e57a31032944fbc1cc4549cd9f5b5a0b789857
stdio2016 <randomfrank8@gmail.com>
2018-12-17 23:57:29 +0800

    padding image and remove bound check

--- Final Project/gaussian_blur_omp.cpp
+++ Final Project/gaussian_blur_omp.cpp
@@ -24,7 +24,7 @@ int FILTER_SIZE;
 ull FILTER_SCALE;
 ull *filter_G;
 
-unsigned char *pic_in, *pic_blur, *pic_out;
+unsigned char *pic_in, *pic_in_padded, *pic_out;
 
 unsigned char gaussian_filter(int w, int h,int shift, int img_border)
 {
@@ -37,15 +37,15 @@ unsigned char gaussian_filter(int w, int h,int shift, int img_border)
 	{
 		for (int i = 0; i  <  ws; i++)
 		{
-			a = w + i - (ws / 2);
-			b = h + j - (ws / 2);
+			a = w + i/* - (ws / 2)*/;
+			b = h + j/* - (ws / 2)*/;
 
 			// detect for borders of the image
-			if (a < 0 || b < 0 || a >= img_width || b >= img_height)
+			/*if (a < 0 || b < 0 || a >= img_width || b >= img_height)
 			{
 				continue;
-			} 
-			tmp += filter_G[j * ws + i] * pic_in[3 * (b * img_width + a) + shift];
+			} */
+			tmp += filter_G[j * ws + i] * pic_in_padded[3 * (b * (img_width+ws) + a) + shift];
 		}
 	}
 	tmp /= FILTER_SCALE;
@@ -60,6 +60,31 @@ unsigned char gaussian_filter(int w, int h,int shift, int img_border)
 	return (unsigned char)tmp;
 	// printf("Input %d Output %d img_birder %d filter_g %f\n", pic_in[3 * (h * img_width + w) + shift], tmp, img_border, filter_G[w % 35]);
 }
+
+unsigned char *padding_pic_for_clamp(unsigned char *pic_in, int img_width, int img_height, int FILTER_SIZE) {
+	int ws = (int)sqrt((int)FILTER_SIZE);
+	int half = (ws-1)/2;
+	ws = half*2+1;
+	unsigned char *pic_out = (unsigned char*) malloc(sizeof(unsigned char[3]) * (img_width + ws) * (img_height + ws));
+	for (int i = 0; i < ws+img_height; i++) {
+		for (int j = 0; j < ws+img_width; j++) {
+			// clamp to edge
+			int y = i - half;
+			int x = j - half;
+			if (y >= img_height) y = img_height - 1;
+			if (y < 0) y = 0;
+			if (x >= img_width) x = img_width - 1;
+			if (x < 0) x = 0;
+			int idx = (i * (ws+img_width) + j) * 3;
+			int orig_idx = (y * img_width + x) * 3;
+			pic_out[idx+0] = pic_in[orig_idx+0];
+			pic_out[idx+1] = pic_in[orig_idx+1];
+			pic_out[idx+2] = pic_in[orig_idx+2];
+		}
+	}
+	return pic_out;
+}
+
 // show the progress of gaussian segment by segment
 const float segment[] = { 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f };
 void write_and_show(BmpReader* bmpReader, string outputblur_name, int k)
@@ -111,6 +136,7 @@ int main(int argc, char* argv[])
 		inputfile_name = argv[k];
 		pic_in = bmpReader -> ReadBMP(inputfile_name.c_str(), &img_width, &img_height);
 		printf("Filter scale = %llu and image size W = %d, H = %d\n", FILTER_SCALE, img_width, img_height);
+		pic_in_padded = padding_pic_for_clamp(pic_in, img_width, img_height, FILTER_SIZE);
 
 		int resolution = 3 * (img_width * img_height);
 		// allocate space for output image
@@ -145,6 +171,7 @@ int main(int argc, char* argv[])
 		write_and_show(bmpReader, outputblur_name, k);
 		// free memory space
 		free(pic_in);
+		free(pic_in_padded);
 		free(pic_out);
 	}
 
